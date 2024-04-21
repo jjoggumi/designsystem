751dbfc36d54d08efebac4769f1a31a55fcb40ed
KEY <40448990+jjoggumi@users.noreply.github.com>
2024-04-21 09:29:33 +0900

    dd

--- src/views/layout/HeadTitle.vue
+++ src/views/layout/HeadTitle.vue
@@ -7,7 +7,7 @@
       <button type="button" class="figma" @click="window.open(headTitle.figmaLink)" v-if="headTitle.figmaLink">figma</button>
       <button type="button" class="scss" @click="window.open(headTitle.scssLink)"  v-if="headTitle.scssLink">scss</button>
       <button type="button" class="js" @click="window.open(headTitle.jsLink)"  v-if="headTitle.jsLink">js</button>
-    </p>
+    </p> 
   </header>
 </template>
 
